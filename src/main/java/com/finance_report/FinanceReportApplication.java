89bb30cb8f6e8b06733cea1aef48e9ce2e89eb8c
sojung.kim <thwjd2717@gmail.com>
2025-05-08 15:21:16 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/finance_report/FinanceReportApplication.java
@@ -0,0 +1,13 @@
+package com.finance_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finance_report/FinanceReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
